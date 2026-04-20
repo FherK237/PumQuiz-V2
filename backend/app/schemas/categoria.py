5a4773e7964ae2acdb47d3a6e6eb2e68c1383a1e
Fernando <ferkev230@gmail.com>
2026-04-20 01:27:22 -0600

    Fixeo a la funcion get categorias

--- backend/app/schemas/categoria.py
+++ backend/app/schemas/categoria.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, ConfigDict
+from typing import Optional
 
 # Esquemas para categorias
 
@@ -13,7 +14,10 @@ class CategoriaCreate(CategoriaBase):
 # 3. Lo que se le responde al cliente después de crear una categoria incluyendo el ID generado
 class CategoriaResponse(CategoriaBase):
     categoria_id: int
-    num_trivias: int
+    num_trivias: Optional[int] = 0
+    
+    class Config:
+        from_attributes = True
 
     # Esto permite que Pydantic lea la respuesta directamente de un modelo SQLAlchemy sin necesidad de convertirlo a un diccionario primero
     model_config = ConfigDict(from_attributes=True)
